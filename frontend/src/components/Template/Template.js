88a3d10fb4627b3a92920aef0b6361d84c10c5c3
Ilaria Schinina <ilaria.schinina@gmail.com>
2022-06-26 11:40:05 +0200

    FE: starting retrieving the template

--- frontend/src/components/Template/Template.js
+++ frontend/src/components/Template/Template.js
@@ -1,4 +1,19 @@
 import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_TEMPLATES = gql`
+  query TemplateQuery ($id: String!) {
+    template(id: $id) {
+      name,
+      groups {
+        name,
+        lang,
+        items,
+        _id
+      },
+      _id
+    }
+  }`;
 
 function Template() {
   let params = useParams();
