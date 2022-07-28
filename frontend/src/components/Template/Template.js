9e3c3a4b576fbe96ddebad927762ae9b6d00798e
Ilaria Schinina <ilaria.schinina@gmail.com>
2022-07-28 18:20:34 +0200

    render groups and their items

--- frontend/src/components/Template/Template.js
+++ frontend/src/components/Template/Template.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
 
-const GET_TEMPLATES = gql`
+const GET_TEMPLATE = gql`
   query TemplateQuery ($id: String!) {
     template(id: $id) {
       name,
@@ -15,10 +15,40 @@ const GET_TEMPLATES = gql`
     }
   }`;
 
-function Template() {
-  let params = useParams();
+
+function Template( {_id} ) {
+  const params = useParams();
+  const { loading, error, data } = useQuery(GET_TEMPLATE, {
+    variables: {
+      id: params.templateId
+    }
+  });
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error </p>;
+  }
+  const { template } = data;
+
+  const templateName = template.name;
+  const groupList = template.groups.map(group => {
+    const itemList = group.items.map(item => (
+      <li> { item } </li>
+    ));
+    return(
+      <div key={ group._id }>
+          { group.name }
+          { itemList }
+      </div>
+    )
+  })
+
   return (
-    <div> I am a template, { params.templateId } </div>
+    <div>
+      <h1> { templateName } </h1>
+      <div>{ groupList }</div>
+    </div>
   );
 }
 
