90840168bba1fdb57c14c94a3b331593d39eae85
Ilaria Schinina <ilaria.schinina@gmail.com>
2022-08-01 15:09:15 +0200

    add title to template list page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import TemplateList from './components/TemplateList/TemplateList'
 function App() {
   return (
     <div className="App">
+      <h1> Your Templates </h1>
       <TemplateList></TemplateList>
     </div>
   );
